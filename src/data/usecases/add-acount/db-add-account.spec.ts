f2ef54ca87e74515c2db9e4fd25899945a91b30d
André <andrepedrobotelho@gmail.com>
2021-02-28 10:26:18 -0300

    refactor: move dbaddAcount sut creation to a factory helper method

--- src/data/usecases/add-acount/db-add-account.spec.ts
+++ src/data/usecases/add-acount/db-add-account.spec.ts
@@ -1,15 +1,29 @@
-import { DBAddAcount } from './db-add-account'
+import { Encrypter } from '../../protocols/encrypter'
+import { DbAddAcount } from './db-add-account'
 
-describe('DBAddAcount UseCase', () => {
-  test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string> {
-        return await new Promise(resolve => resolve('hashed_password'))
-      }
+interface DbAddAcountTypes{
+  dbAddAcount: DbAddAcount
+  encrypterStub: Encrypter
+}
+
+const makeDbAddAcount = (): DbAddAcountTypes => {
+  class EncrypterStub {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => resolve('hashed_password'))
     }
-    const encrypterStub = new EncrypterStub()
+  }
+  const encrypterStub = new EncrypterStub()
 
-    const dBAddAcount = new DBAddAcount(encrypterStub)
+  const dbAddAcount = new DbAddAcount(encrypterStub)
+
+  return {
+    dbAddAcount,
+    encrypterStub
+  }
+}
+describe('DbAddAcount UseCase', () => {
+  test('Should call Encrypter with correct password', async () => {
+    const { encrypterStub, dbAddAcount } = makeDbAddAcount()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
 
     const accountData = {
@@ -18,7 +32,7 @@ describe('DBAddAcount UseCase', () => {
       password: 'valid_password'
     }
 
-    await dBAddAcount.add(accountData)
+    await dbAddAcount.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
 })
--- src/data/usecases/add-acount/db-add-account.ts
+++ src/data/usecases/add-acount/db-add-account.ts
@@ -2,7 +2,7 @@ import { AccountModel } from './../../../domain/models/account'
 import { AddAccount, AddAccountModel } from './../../../domain/usecases/add-account'
 import { Encrypter } from '../../protocols/encrypter'
 
-export class DBAddAcount implements AddAccount {
+export class DbAddAcount implements AddAccount {
   private readonly encrypter: Encrypter
 
   constructor (encrypter: Encrypter) {
