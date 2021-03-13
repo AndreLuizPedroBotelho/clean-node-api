600ce6beae4e990724f56de68376e7116a0b149d
André <andrepedrobotelho@gmail.com>
2021-03-12 22:21:52 -0300

    test: ensure LoginController call EmailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,16 +1,30 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
+import { EmailValidator } from '../signup/signup-protocols'
 import { LoginController } from './login'
 
+const makeEMailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  return new EmailValidatorStub()
+}
 interface LoginControllerTypes{
   loginController: LoginController
+  emailValidatorStub: EmailValidator
 }
 
 const makeLoginController = (): LoginControllerTypes => {
-  const loginController = new LoginController()
+  const emailValidatorStub = makeEMailValidator()
+
+  const loginController = new LoginController(emailValidatorStub)
 
   return {
-    loginController
+    loginController,
+    emailValidatorStub
   }
 }
 describe('Login Controller', () => {
@@ -39,5 +53,35 @@ describe('Login Controller', () => {
     const httpResponse = await loginController.handle(httpRequest)
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should call EmailValidator with correct email ', async () => {
+    const { loginController, emailValidatorStub } = makeLoginController()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    await loginController.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('Should return 400 if an invalid email is provided', async () => {
+    const { loginController, emailValidatorStub } = makeLoginController()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await loginController.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
 })
