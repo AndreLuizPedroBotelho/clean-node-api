db31ec676ddd62bac7ee89794ea895b0ec25a9bf
André <andrepedrobotelho@gmail.com>
2021-03-14 20:44:06 -0300

    test: ensure AccountMongoRepository return an account on loadByEmail success and return an null with fail

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,5 +1,8 @@
+import { Collection } from 'mongodb'
 import { MongoHelper } from './../../mongodb/helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
+
+let accountCollection: Collection
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
@@ -10,7 +13,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
 
     await accountCollection.deleteMany({})
   })
@@ -18,7 +21,15 @@ describe('Account Mongo Repository', () => {
   const makeAccountMongoRepository = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
-  test('Should return an account on success', async () => {
+
+  const makeFakeAccount = async (): Promise<void> => {
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+  }
+  test('Should return an account on add success', async () => {
     const accountMongoRepository = makeAccountMongoRepository()
     const account = await accountMongoRepository.add({
       name: 'any_name',
@@ -32,5 +43,26 @@ describe('Account Mongo Repository', () => {
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
+  })
+
+  test('Should return an account on loadByEmail success', async () => {
+    const accountMongoRepository = makeAccountMongoRepository()
+
+    await makeFakeAccount()
+    const account = await accountMongoRepository.loadByEmail('any_email@mail.com')
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+  test('Should return null if loadByEmail', async () => {
+    const accountMongoRepository = makeAccountMongoRepository()
+
+    const account = await accountMongoRepository.loadByEmail('any_email@mail.com')
+
+    expect(account).toBeFalsy()
   })
 })
