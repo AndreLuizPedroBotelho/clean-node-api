c5992fcbd51387466f0d84bae1752f2f7103cbd6
André <andrepedrobotelho@gmail.com>
2021-03-14 22:45:13 -0300

    test: login route returns 401 with invalid credentials

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,5 +4,5 @@ import env from './config/env'
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import ('./config/app')).default
-    app.listen(env.port, () => console.log(`Server running ar http:localhost:${env.port}`))
+    app.listen(env.port, () => console.log(`Server running ar http://localhost:${env.port}`))
   }).catch(console.error)
