372e4bf26c471c400bd79ab52e0217e3a7127a1f
André <andrepedrobotelho@gmail.com>
2021-03-14 22:34:13 -0300

    feat: add LoginFactory

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,15 +1,33 @@
-import { LoginController } from '../../../presentation/controllers/login/login-controller'
-import { Controller } from '../../../presentation/protocols'
-import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+import { JwtAdapter } from './../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { BcryptAdapter } from './../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { AccountMongoRepository } from './../../../infra/db/mongodb/account/account-mongo-repository'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 
+import { Controller } from '../../../presentation/protocols'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+
 import { makeLoginValidation } from './login-validation-factory'
-import { Authentication } from '../../../domain/usecases/authentication'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import env from '../../config/env'
 
 export const makeLoginController = (): Controller => {
+  const salt = 12
+
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+
+  const dbAuthentication = new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository
+  )
+
   const logMongoRepository = new LogMongoRepository()
 
-  const signUpController = new LoginController(null as unknown as Authentication, makeLoginValidation())
+  const loginController = new LoginController(dbAuthentication, makeLoginValidation())
 
-  return new LogControllerDecorator(signUpController, logMongoRepository)
+  return new LogControllerDecorator(loginController, logMongoRepository)
 }
