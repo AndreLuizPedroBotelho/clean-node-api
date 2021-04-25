0db1b1e93d7d7b465759f51de809a9bc459a3cf0
André <andrepedrobotelho@gmail.com>
2021-04-25 15:17:29 -0300

    test: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -9,11 +9,12 @@ const makeAccountMongoRepository = (): AccountMongoRepository => {
   return new AccountMongoRepository()
 }
 
-const makeFakeAccount = async (): Promise<AccountModel> => {
+const makeFakeAccount = async (accessToken?: string): Promise<AccountModel> => {
   const res = await accountCollection.insertOne({
     name: 'any_name',
     email: 'any_email@mail.com',
-    password: 'any_password'
+    password: 'any_password',
+    accessToken
   })
 
   return MongoHelper.map(res.ops[0])
@@ -93,6 +94,22 @@ describe('Account Mongo Repository', () => {
 
       expect(account).toBeTruthy()
       expect(account.accessToken).toBe('any_token')
+    })
+  })
+
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken success without role', async () => {
+      const accountMongoRepository = makeAccountMongoRepository()
+
+      await makeFakeAccount('any_token')
+      const account = await accountMongoRepository.loadByToken('any_token')
+
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
   })
 })
