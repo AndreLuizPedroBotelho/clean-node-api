e11439761509729502e6109df52187239620e3bd
André <andrepedrobotelho@gmail.com>
2021-04-27 23:29:33 -0300

    test: ensure LoadSurveysRepository call LoadSurveysRepository findAll and throws

---
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -0,0 +1,86 @@
+import { SurveyModel, LoadSurveysRepository } from './db-load-surveys-protocols'
+import { DbLoadSurveys } from './db-load-surveys'
+import MockDate from 'mockdate'
+
+interface DbAddSurveyTypes{
+  dbLoadSurveys: DbLoadSurveys
+  loadSurveysRepositoryStub: LoadSurveysRepository
+}
+
+const makeFakeSurveys = (): SurveyModel[] => ([
+  {
+    id: 'any_id',
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }],
+    date: new Date()
+  },
+  {
+    id: 'other_id',
+    question: 'other_question',
+    answers: [{
+      image: 'other_image',
+      answer: 'other_answer'
+    }],
+    date: new Date()
+  }])
+
+const makeLoadSurveysRepository = (): LoadSurveysRepository => {
+  class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+    async loadAll (): Promise<SurveyModel[]> {
+      return await new Promise(resolve => resolve(makeFakeSurveys()))
+    }
+  }
+
+  return new LoadSurveysRepositoryStub()
+}
+
+const makeDbAddSurvey = (): DbAddSurveyTypes => {
+  const loadSurveysRepositoryStub = makeLoadSurveysRepository()
+  const dbLoadSurveys = new DbLoadSurveys(loadSurveysRepositoryStub)
+
+  return {
+    dbLoadSurveys,
+    loadSurveysRepositoryStub
+  }
+}
+describe('DbLoadSurveys UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call LoadSurveysRepository ', async () => {
+    const {
+      dbLoadSurveys,
+      loadSurveysRepositoryStub
+    } = makeDbAddSurvey()
+
+    const loadSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
+
+    await dbLoadSurveys.load()
+
+    expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should throw if LoadSurveysRepository throw', async () => {
+    const {
+      dbLoadSurveys,
+      loadSurveysRepositoryStub
+    } = makeDbAddSurvey()
+
+    jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
+      .mockReturnValue(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+
+    const promise = dbLoadSurveys.load()
+
+    await expect(promise).rejects.toThrow()
+  })
+})
---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,12 @@
+import { LoadSurveys, SurveyModel, LoadSurveysRepository } from './db-load-surveys-protocols'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (
+    private readonly loadSurveysRepository: LoadSurveysRepository
+  ) {}
+
+  async load (): Promise<SurveyModel[]> {
+    const surveys = await this.loadSurveysRepository.loadAll()
+    return surveys
+  }
+}
