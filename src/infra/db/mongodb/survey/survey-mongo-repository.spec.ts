6613ccc6ead821a95df6f8c0cc0c11663091287c
André <andrepedrobotelho@gmail.com>
2021-04-27 23:55:09 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -3,6 +3,11 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { SurveyMongoRepository } from './survey-mongo-repository'
 
 let surveyCollection: Collection
+
+const makeSurveyMongoRepository = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
+
 describe('Survey Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
@@ -18,25 +23,54 @@ describe('Survey Mongo Repository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  const makeSurveyMongoRepository = (): SurveyMongoRepository => {
-    return new SurveyMongoRepository()
-  }
+  describe('add()', () => {
+    test('Should return an survey on add success', async () => {
+      const surveyMongoRepository = makeSurveyMongoRepository()
+      await surveyMongoRepository.add({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      })
 
-  test('Should return an survey on add success', async () => {
-    const surveyMongoRepository = makeSurveyMongoRepository()
-    await surveyMongoRepository.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
-      },
-      {
-        answer: 'any_answer'
-      }],
-      date: new Date()
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
+  })
 
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  describe('loadAll()', () => {
+    test('Should return an list survey', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      }, {
+        question: 'other_question',
+        answers: [{
+          image: 'other_image',
+          answer: 'other_answer'
+        },
+        {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }])
+
+      const surveyMongoRepository = makeSurveyMongoRepository()
+      const surveys = await surveyMongoRepository.loadAll()
+
+      expect(surveys.length).toBe(2)
+    })
   })
 })
