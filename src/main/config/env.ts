7de25de1b9cc60b852fd3d7a14dd060ee7b7841a
André <andrepedrobotelho@gmail.com>
2021-04-30 00:36:05 -0300

    refactor: allows api to run locally or with docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL as string || 'mongodb://mongo:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL as string || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT as string || 5000,
   jwtSecret: process.env.JWT_SECRET as string || 'JmthzO&TMGoy'
 }
