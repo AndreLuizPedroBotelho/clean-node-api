54e6d40f6a4eb2597df98c4db2cbb38288606db1
André <andrepedrobotelho@gmail.com>
2021-09-17 23:52:53 -0300

    feat:ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,17 @@
+import {
+  LoadSurveyResult,
+  SurveyResultModel,
+  LoadSurveyResultRepository
+} from './db-load-survey-result-protocols'
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+  constructor(
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+  ) { }
+
+  async load(surveyId: string): Promise<SurveyResultModel> {
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+
+    return surveyResult
+  }
+}
