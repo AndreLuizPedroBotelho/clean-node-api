9f88955ed45fe18ecf545df74960615c126947b3
André <andrepedrobotelho@gmail.com>
2021-09-19 00:29:37 -0300

    feat:ensure DbLoadSurveyResult returns empty syrvey result if there is no result

--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -8,6 +8,23 @@ export const mockSurveyResultParams = (): SaveSurveyResultParams => ({
   date: new Date()
 })
 
+export const mockSurveyResultEmptyAnswer = (): SurveyResultModel => ({
+  surveyId: 'any_survey_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer',
+    count: 0,
+    percent: 0
+  },
+  {
+    answer: 'other_answer',
+    count: 0,
+    percent: 0
+  }],
+  date: new Date()
+})
+
 export const mockSurveyResultModel = (): SurveyResultModel => ({
   surveyId: 'any_survey_id',
   question: 'any_question',
@@ -23,5 +40,4 @@ export const mockSurveyResultModel = (): SurveyResultModel => ({
     percent: 80
   }],
   date: new Date()
-
 })
--- src/domain/test/mock-survey.ts
+++ src/domain/test/mock-survey.ts
@@ -4,14 +4,17 @@ import { SurveyModel } from '@/domain/models/survey'
 export const mockSurveyParams = (): AddSurveyParams => ({
   question: 'any_question',
   answers: [{
-    image: 'any_image',
-    answer: 'any_answer'
+    answer: 'any_answer',
+    image: 'any_image'
+  },
+  {
+    answer: 'other_answer'
   }],
   date: new Date()
 })
 
 export const mockSurveyModel = (): SurveyModel => ({
-  id: 'any_id',
+  id: 'any_survey_id',
   ...mockSurveyParams()
 })
 
